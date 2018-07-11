903f9e6a17343237d7cef83510e4677473d4d251
stephavos <39690218+stephavos@users.noreply.github.com>
2018-07-11 13:03:00 +0200

    Javadoc add

--- Tron.java
+++ Tron.java
@@ -6,62 +6,72 @@ import Model.Player;
 import View.GameFrame;
 
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Tron.
+ */
 public class Tron {
-	/*
+	/**
 	 * setting of the game
-	 */
+	 * 
+	 *The pixelsize. */
 	
-	/*
-	 * Size of pixel
-	 */
 	public static int PIXELSIZE = 2;
-	
-	/*
+	/**
 	 * coordinated playeurs
-	 */
+	 *
+	 * The P 1 start X. */
+	
 	public static int P1StartX = 600;
+	
+	/** The P 1 start Y. */
 	public static int P1StartY = 200;
 	
+	/** The P 2 start X. */
 	public static int P2StartX = 0;
+	
+	/** The P 2 start Y. */
 	public static int P2StartY = 200;
 	
-	//Orientation de départ(Player.LEFT/RIGHT/UP/DOWN)
+	/** The p 1 init orientation. */
+
 	public static int p1InitOrientation = Player.LEFT;
+	
+	/** The p 2 init orientation. */
 	public static int p2InitOrientation = Player.RIGHT;
 	
-	/*
-	 * size of the game window
-	 */
+	/** The gridw.size of the game window */
+	
 	public static int GRIDW = 600;
+	
+	/** The gridh. */
 	public static int GRIDH = 400;
 	
-	/*
-	 * RGB color of the 2 players
-	 */
-	public static Color p1Color = new Color(3,3,173); 
-	/*
-	 * blue
-	 */
+	/** The p 1 color. Blue */
 	
+	public static Color p1Color = new Color(3,3,173); 
+	
+	/** The p 2 color. Red */
 	public static Color p2Color = new Color(173,3,3); 
-	/*
-	 * Red
-	 */
+	
 
 	
-	/*
-	 * Nickname of the players
-	 */
+	/** The playeur 1 name. */
+	
 	public static String p1Name = "Blue";
+	
+	/** The playeur 2 name. */
 	public static String p2Name = "Red";
 	
-/*
- * Motion type (Pivot: use the Q, D and <-, -> keys to change angles (false), Direction: use direction keys (true).
- */
+/** The Gametype. p1= <-/-> and p2= Q/D*/
+
 	public static boolean Gametype = false;
 	
-	
-	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		
 		 System.out.println("-> Gride size: " + GRIDW + " X " + GRIDH);
